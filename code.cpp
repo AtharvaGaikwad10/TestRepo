4115eebc69712ce02c66f5107337f49d6616947b
AtharvaGaikwad10 <atharvaagaikwad1011@gmail.com>
2026-08-18 23:48:41 +0530

    New code written

---
+++ code.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Student{
+
+    int PRN;
+    string name;
+    string dept;
+
+};
+
+int main(){
+    cout<<"This code is written by me";
+    return 0;
+}
